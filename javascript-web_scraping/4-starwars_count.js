d719ed33bc80a738d1e2ca1a6a5e793b3d0e3919
Ferlando <contact@ferlando.me>
2024-05-13 14:39:48 +0200

    add ex 4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/local/bin/node
+
+const request = require('request');
+const url = `${process.argv[2]}`;
+
+request(url, (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const info = JSON.parse(data.body);
+    let count = 0;
+    for (const result in info.results) {
+      const characters = info.results[result].characters;
+      for (const character in characters) {
+        if (characters[character].endsWith('18/')) {
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
