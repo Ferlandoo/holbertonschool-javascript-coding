6ae377964578f9abf78f69fa42490e3919712e46
Ferlando <contact@ferlando.me>
2024-05-13 15:20:28 +0200

    add ex 5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const fileName = `${process.argv[3]}`;
+
+request(process.argv[2], 'utf-8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const info = data.body;
+    fs.writeFile(fileName, info, (error) => {
+      if (error) {
+        console.log(error);
+      }
+    })
+  }
+})
