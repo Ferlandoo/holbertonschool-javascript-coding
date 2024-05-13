1150f30b07ebffc3e94f369502de66908ef8dd3e
Ferlando <contact@ferlando.me>
2024-05-13 10:56:54 +0200

    add ex 2

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/local/bin/node
+
+const https = require('https');
+const address = process.argv[2];
+
+https.get(address, (result) => {
+  console.log(`code: ${result.statusCode}`);
+  process.exit();
+})
