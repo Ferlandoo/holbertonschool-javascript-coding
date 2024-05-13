768d33bbd11515722f3b5698f2a4c069cf357ca9
Ferlando <contact@ferlando.me>
2024-05-13 14:07:23 +0200

    ADD EX 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/local/bin/node
+
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+
+request(url, (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const info = JSON.parse(data.body);
+    console.log(info.title);
+  }
+});
