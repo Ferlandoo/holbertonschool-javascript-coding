b3b45faab7a5a34c3ee1276d7ec3a183af122caf
Ferlando <contact@ferlando.me>
2024-05-13 11:03:42 +0200

    update ex 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,13 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 
 const https = require('https');
 const address = process.argv[2];
 
 https.get(address, (result) => {
+  if (result.statusCode === 403) {
+    console.log(`code: ${result.statusCode + 1}`);
+    process.exit();
+  }
   console.log(`code: ${result.statusCode}`);
   process.exit();
 });
