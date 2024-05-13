6df997cf27f6b49efeeda9269dfd1616adfeebf8
Ferlando <contact@ferlando.me>
2024-05-13 10:31:46 +0200

    add ex 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/local/bin/node
+
+const fs = require('fs');
+const textArg = process.argv[3];
+const fileName = process.argv[2];
+
+fs.writeFile(fileName, textArg, 'utf-8', (error) => {
+  if (error) {
+    console.log(error);
+  }
+});
