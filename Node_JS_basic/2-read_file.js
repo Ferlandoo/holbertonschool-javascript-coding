1efcb1f6f16e56d6df655f27fa5a80a335d512e8
Ferlando <contact@ferlando.me>
2024-05-23 18:25:02 +0200

    update eslint

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,14 +1,14 @@
 function countStudents(argPath) {
   const fs = require('fs');
-  fs.readFile(argPath, 'utf8', function (error, data) {
+  fs.readFile(argPath, 'utf8', (error, data) => {
     if (error) {
       throw new Error('Cannot load the database');
     }
-    let lines = data.split('\n');
+    const lines = data.split('\n');
     lines.splice(0, 1);
     console.log(`Number of students: ${lines.length}`);
-    let splited = lines.map((word) => word.split(","));
-    let subjects = {};
+    const splited = lines.map((word) => word.split(','));
+    const subjects = {};
     for (let i = 0; i < splited.length; i++) {
       if (!subjects[splited[i][3]]) {
         subjects[splited[i][3]] = [];
@@ -17,7 +17,7 @@ function countStudents(argPath) {
     }
     console.log(`Number of students in ${Object.keys(subjects)[0]}: ${subjects.CS.length}. List: ${subjects.CS.join(', ')}`);
     console.log(`Number of students in ${Object.keys(subjects)[1]}: ${subjects.SWE.length}. List: ${subjects.SWE.join(', ')}`);
-  })
+  });
 }
 
 module.exports = countStudents;
